119036114c6d135242862dfaa9d92409fb08b096
Andrea Soranzo <andreajsm03@gmail.com>
2024-11-04 17:50:53 +0100

    sdsd

---
+++ prov.py
@@ -0,0 +1,15 @@
+import sys as sys
+import shutil as cmd
+import pathlib as l
+
+def main() :
+    cmd.rmtree('dist', ignore_errors=True)
+    cmd.copytree('website', 'dist', symlinks=False)
+
+    html = l.Path('dist/index.html').read_text()
+
+    html = html.replace('{{HELLO}}',"AAAAAAAAAAAAAAAAAAAAAAAAAAAAA") 
+
+if __name__ == "__main__":
+    main()
+
