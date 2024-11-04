4d6150ea52b1cfa551008b603ec871580323ddb3
Andrea Soranzo <andreajsm03@gmail.com>
2024-11-04 20:04:55 +0100

    sd

--- prov.py
+++ prov.py
@@ -3,12 +3,15 @@ import shutil as cmd
 import pathlib as l
 import subprocess
 import os
+import logging
 
 from pdflatex import PDFLaTeX
 
 TEMPLATE = '<li><a href="{{link}}" target="_blank">{{name}}</a></li>'
 
 def main() :
+    logging.basicConfig(level=os.getenv('LOGLEVEL', 'INFO'))
+
     cmd.rmtree('_site', ignore_errors=True)
     cmd.copytree('website', '_site', symlinks=False)
 
@@ -18,7 +21,7 @@ def main() :
         pdfl = PDFLaTeX.from_texfile('letex/'+item)
         pdfl.set_output_directory("_site")
         pdf, _ , completed_process = pdfl.create_pdf(keep_pdf_file=True, keep_log_file=False)  
-        print('letex/'+item)
+        logging.info('letex/'+item)
         if completed_process.returncode == 0:
             pdfs.append(item.replace(".tex",".pdf"))
 
